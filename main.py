8f2d85dd5c812ec9b14173a5f186d446e434208d
nologicexe <nologicex@gmail.com>
2021-04-23 01:21:20 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import pyaudio
+import wave
+import numpy as np
+from matplotlib import pyplot as plt
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+
+# audio_data = np.array(data)
+# audio_data.reshape((1, data.shape[0]*data.shape[1]))
+print(data)
+plt.plot(data[0])
+plt.show();
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
